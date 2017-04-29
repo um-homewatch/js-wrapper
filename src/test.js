bf74669df0de7ea77b10df864b4a609d0878357e
José Sousa <josesousa9000@gmail.com>
2017-04-29 01:02:15 +0100

    Use ES7 async

--- src/test.js
+++ src/test.js
@@ -2,13 +2,15 @@ Homewatch = require("./api.js");
 
 homewatch = new Homewatch("http://localhost:3000");
 
-homewatch.users.register("José", "josesousa9000@gmail.com", "123456").
-  then((result) => {
-    console.log(result.data);
-    let jwt = result.data.jwt;
-    homewatch.auth = jwt;
-    homewatch.users.currentUser().then((result) => console.log(result.data));
-  }, (err) => {
-    console.log(err.response.data); // Error: "It broke"
-  });
+async function main() {
+  try {
+    let user = await homewatch.users.register("José", "josesousa9000@gmail.com", "123456")
+    homewatch.auth = user.data.jwt;
+    let currentUser = await homewatch.users.currentUser()
+    console.log(currentUser.data);
+  } catch (err) {
+    console.error(err);
+  }
+}
 
+main();
--- test/users_test.js
+++ test/users_test.js
@@ -3,74 +3,45 @@ const faker = require("faker");
 const Homewatch = require("../src/api");
 const homewatch = new Homewatch("http://localhost:3000");
 
-describe("Array", function() {
-  describe("user registration", function() {
-    it("should register a user", function(done) {
-      user = generateUser();
-      homewatch.users.register(user.name, user.email, user.password)
-        .then((response) => {
-          expect(response.data.name).to.eq(user.name);
-          expect(response.data.email).to.eq(user.email);
-          done();
-        }, (error) => {
-          done(error.response.data);
-        })
-        .catch((err) => done(err));
-    });
+describe("users endpoint", function() {
+  let USER;
+
+  it("should register a user", async () => {
+    user = generateUser();
+
+    let response = await homewatch.users.register(user.name, user.email, user.password);
+    expect(response.data.name).to.eq(user.name);
+    expect(response.data.email).to.eq(user.email);
   });
 
-  describe("user self methods", () => {
-    let USER;
 
-    beforeEach((done) => {
-      USER = generateUser();
-      homewatch.users.register(USER.name, USER.email, USER.password)
-        .then((response) => {
-          USER = response.data;
-          done();
-        }, (error) => {
-          done(error.response.data);
-        })
-        .catch((err) => done(err));
-    });
+  beforeEach(async () => {
+    let user = generateUser();
+    let response = await homewatch.users.register(user.name, user.email, user.password);
+    USER = response.data;
+  });
 
-    it("should login a user", (done) => {
-      homewatch.users.login(user.email, user.password)
-        .then((response) => {
-          expect(response.data.jwt).to.exist;
-          done();
-        }, (error) => {
-          done(error.response.data);
-        })
-        .catch((err) => done(err));
-    });
+  it("should login a user", async () => {
+    let response = await homewatch.users.login(user.email, user.password);
 
-    it("should update a user", (done) => {
-      homewatch.auth = USER.jwt;
-      user = generateUser();
-      homewatch.users.updateCurrentUser(user.name, user.email, user.password)
-        .then((response) => {
-          expect(response.data.name).to.eq(user.name);
-          expect(response.data.email).to.eq(user.email);
-          done();
-        }, (error) => {
-          done(error.response.data);
-        })
-        .catch((err) => done(err));
-    });
+    expect(response.data.jwt).to.exist;
+  });
 
-    it("should return the user data", (done) => {
-      homewatch.auth = USER.jwt;
-      homewatch.users.currentUser()
-        .then((response) => {
-          expect(response.data.name).to.eq(USER.name);
-          expect(response.data.email).to.eq(USER.email);
-          done();
-        }, (error) => {
-          done(error.response.data);
-        })
-        .catch((err) => done(err));
-    });
+  it("should update a user", async () => {
+    homewatch.auth = USER.jwt;
+    user = generateUser();
+    let response = await homewatch.users.updateCurrentUser(user.name, user.email, user.password);
+
+    expect(response.data.name).to.eq(user.name);
+    expect(response.data.email).to.eq(user.email);
+  });
+
+  it("should return the user data", async () => {
+    homewatch.auth = USER.jwt;
+    let response = await homewatch.users.currentUser();
+
+    expect(response.data.name).to.eq(USER.name);
+    expect(response.data.email).to.eq(USER.email);
   });
 });
 
